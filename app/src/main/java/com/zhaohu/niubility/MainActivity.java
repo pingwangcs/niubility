554ba35c7b7377684660e97fba2442f4378353dc
wen <wenzhao@iastate.edu>
2015-01-11 12:19:40 -0800

    Simulate wechat UI with three tabs at top. Looks REALLY good. But we should change UI details later.
    Ref:http://blog.csdn.net/guolin_blog/article/details/26365683

---
+++ app/src/main/java/com/zhaohu/niubility/MainActivity.java
@@ -0,0 +1,178 @@
+package com.zhaohu.niubility;
+
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewConfiguration;
+import android.view.Window;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.zhaohu.niubility.fragments.BookmarksFragment;
+import com.zhaohu.niubility.fragments.HomeFragment;
+import com.zhaohu.niubility.fragments.PhotoWallFragment;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+
+public class MainActivity extends FragmentActivity {
+
+    /**
+     * 首页界面的Fragment
+     */
+    private HomeFragment homeFragment;
+
+    /**
+     * 照片墙界面的Fragment
+     */
+    private PhotoWallFragment photoWallFragmentFragment;
+
+    /**
+     * 收藏夹界面的Fragment
+     */
+    private BookmarksFragment bookmarksFragment;
+
+    private PagerSlidingTabStrip tabs;
+
+    private DisplayMetrics dm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setOverflowShowingAlways();
+
+        dm = getResources().getDisplayMetrics();
+
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        tabs.setViewPager(pager);
+
+        setTabsValue();
+    }
+
+    private void setTabsValue() {
+        // 设置Tab是自动填充满屏幕的
+        tabs.setShouldExpand(true);
+        // 设置Tab的分割线是透明的
+        tabs.setDividerColor(Color.TRANSPARENT);
+        // 设置Tab底部线的高度
+        tabs.setUnderlineHeight((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 1, dm));
+        // 设置Tab Indicator的高度
+        tabs.setIndicatorHeight((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 4, dm));
+        // 设置Tab标题文字的大小
+        tabs.setTextSize((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP, 16, dm));
+        // 设置Tab Indicator的颜色
+        tabs.setIndicatorColor(Color.parseColor("#45c01a"));
+        // 设置选中Tab文字的颜色 (这是我自定义的一个方法)
+        tabs.setSelectedTextColor(Color.parseColor("#45c01a"));
+        // 取消点击Tab时的背景色
+        tabs.setTabBackground(0);
+    }
+
+    private void setOverflowShowingAlways() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class
+                    .getDeclaredField("sHasPermanentMenuKey");
+            menuKeyField.setAccessible(true);
+            menuKeyField.setBoolean(config, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        private final String[] titles = { "首页", "照片墙", "收藏夹" };
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    if (homeFragment == null) {
+                        homeFragment = new HomeFragment();
+                    }
+                    return homeFragment;
+                case 1:
+                    if (photoWallFragmentFragment == null) {
+                        photoWallFragmentFragment = new PhotoWallFragment();
+                    }
+                    return photoWallFragmentFragment;
+                case 2:
+                    if (bookmarksFragment == null) {
+                        bookmarksFragment = new BookmarksFragment();
+                    }
+                    return bookmarksFragment;
+                default:
+                    return null;
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod(
+                            "setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+}
